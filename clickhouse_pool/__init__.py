81775e74c4ecf0404be68e209cadc26b5336b4ba
Eric McCarthy <ericmccarthy7@gmail.com>
2020-06-23 17:11:25 -0400

    update so package is working

---
+++ clickhouse_pool/__init__.py
@@ -0,0 +1,4 @@
+from .pool import ChPool
+
+__version__ = "0.1.1"
+__all__ = ["ChPool"]
--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
+import codecs
+import os.path
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setuptools.setup(
     name="clickhouse-pool",
-    version="0.1.0",
+    version=get_version("clickhouse_pool/__init__.py"),
     author="Eric McCarthy",
     author_email="ericmccarthy7@gmail.com",
     description="A thread-safe connection pool for ClickHouse.",
