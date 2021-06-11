188a4162d6c246a911db6553193cf8ed4731b362
Eric McCarthy <ericmccarthy7@gmail.com>
2021-06-11 13:13:43 -0400

    remove clickhouse_host env variable

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -1,16 +1,9 @@
-import os
-
 import pytest
 from clickhouse_pool.pool import ChPool, TooManyConnections
 
 
-@pytest.fixture(scope="session")
-def clickhouse_host():
-    return os.getenv("TEST_CLICKHOUSE_HOST", "localhost")
-
-
-def test_context(clickhouse_host):
-    pool = ChPool(host=clickhouse_host)
+def test_context():
+    pool = ChPool()
     with pool.get_client() as client:
         result = client.execute("SELECT * FROM system.numbers LIMIT 5;")
         assert result == [(0,), (1,), (2,), (3,), (4,)]
@@ -19,8 +12,8 @@ def test_context(clickhouse_host):
     pool.cleanup()
 
 
-def test_connections_min(clickhouse_host):
-    pool = ChPool(host=clickhouse_host, connections_min=5, connections_max=10)
+def test_connections_min():
+    pool = ChPool(connections_min=5, connections_max=10)
     clients = []
     for i in range(1, 6):
         clients.append(pool.pull())
@@ -35,8 +28,8 @@ def test_connections_min(clickhouse_host):
     pool.cleanup()
 
 
-def test_connections_max(clickhouse_host):
-    pool = ChPool(host=clickhouse_host, connections_min=1, connections_max=3)
+def test_connections_max():
+    pool = ChPool(connections_min=1, connections_max=3)
     clients = []
     for i in range(3):
         clients.append(pool.pull())
@@ -49,8 +42,8 @@ def test_connections_max(clickhouse_host):
     pool.cleanup()
 
 
-def test_connection_released_on_error(clickhouse_host):
-    pool = ChPool(host=clickhouse_host)
+def test_connection_released_on_error():
+    pool = ChPool()
     try:
         with pool.get_client() as client:
             client.execute("some_invalid_sql")
